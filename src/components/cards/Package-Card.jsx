7b0e725a2b67402df4a21488869af8925a3cd808
CheshtaChawla <chawlacheshta55@gmail.com>
2023-09-19 12:54:23 +0530

    backup-code-cheshta

--- src/components/cards/Package-Card.jsx
+++ src/components/cards/Package-Card.jsx
@@ -7,7 +7,7 @@ const PackageCard = ({ title, description, imageUrl, customSize, customBackgroun
   // const background_color = customBackground? '#000000': '#ffffff';
   return (
     <div className="card" >
-      <img src={imageUrl} alt="Card" className="card-image" style={{ width: '100%', height: imageHeight }} />
+      <img src={imageUrl} alt="Card"  style={{ width: '100%', height: imageHeight }} />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
